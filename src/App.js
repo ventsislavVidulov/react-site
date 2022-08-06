08cbfbc0df8ae3d554dcf140854c84bad3edf9c6
ventsislavVidulov <xcen3k@gmail.com>
2022-08-06 18:24:49 +0300

    Update login and register functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import logo from './logo.svg';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
 import './App.css';
 import Header from './components/Header/Header'
+import Login from './components/Login/Login'
+import Logout from './components/Logout/Logout'
+import Register from './components/Register/Register'
+import { AuthContext } from './contexts/AuthContext'
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 function App() {
+  const [auth, setAuth] = useLocalStorage('auth', {});
+
+
+  const userLogin = (authData) => {
+    setAuth(authData);
+  };
+
+  const userLogout = () => {
+    setAuth({});
+  };
+
+
+
   return (
-    <Header />
+    <AuthContext.Provider value={{ user: auth, userLogin, userLogout }}>
+      <Header />
+      <>
+        <Routes>
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/logout" element={<Logout />} />
+
+        </Routes>
+      </>
+    </AuthContext.Provider>
   );
 }
 
