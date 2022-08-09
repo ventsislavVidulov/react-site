f8bf6fbc058cff9855c2eb5a644dca5c8902e792
ventsislavVidulov <xcen3k@gmail.com>
2022-08-09 16:10:17 +0300

    update css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
-//import { AuthContext } from '../../contexts/AuthContext';
+import './Header.module.css'
+
+import { AuthContext } from '../../contexts/AuthContext';
 
 const Header = () => {
-    // const { user } = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
 
     return (
         <header>
@@ -14,18 +16,18 @@ const Header = () => {
                 </Link>
             </h1>
             <nav>
-                {/* {user.email && <span>{user.email}</span>} */}
+               
                 <Link to="/jokes">JOKES</Link>
-                {/* {user.email */}
-                <div id="user">
-                    <Link to="/create">CREATE JOKE</Link>
-                    <Link to="/logout">LOGOUT</Link>
-                </div>
-                <div id="guest">
-                    <Link to="/login">LOGIN</Link>
-                    <Link to="/register">REGISTER</Link>
-                </div>
-                {/* } */}
+                {user.email
+                    ? <div id="user">
+                        <Link to="/create">CREATE JOKE</Link>
+                        <Link to="/logout">LOGOUT</Link>
+                    </div>
+                    : <div id="guest">
+                        <Link to="/login">LOGIN</Link>
+                        <Link to="/register">REGISTER</Link>
+                    </div>
+                }
             </nav>
         </header>
     );
